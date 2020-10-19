21a65c37e7ee625e4c77ba26bed713b3127e1a1b
dianhuaphreaker <ericqyhan@outlook.com>
2020-10-18 21:53:15 -0400

    HTML lesson level 4 and 5

---
+++ 14-Strings/151-RandomNumbersAndTheMathObj.js
@@ -0,0 +1,34 @@
+// Math Object
+// object - collection of properties and methods
+// Pi is an object that we always have access to
+
+console.log(Math.PI);
+console.log(Math.E);
+
+// Math.floor will take a number and chop off the decimals
+console.log(Math.floor(23.443));
+console.log(Math.floor(23.9999));
+
+// Math.ceil will round it up
+console.log(Math.ceil(23.1)); //24
+console.log(Math.ceil(23.01)); //24
+
+// Math.random will give you a random number between 0 and 1
+console.log(Math.random());
+console.log(Math.random());
+
+// Math.random
+console.log(Math.floor(Math.random() * 12));
+console.log(Math.floor(Math.random() * 12) + 1);
+console.log(Math.floor(Math.random() * 12) + 1);
+console.log(Math.floor(Math.random() * 12) + 1);
+
+// Math.random - if you want to go between two numbers
+console.log(Math.floor(Math.random() * 3) + 20);
+
+// Math.pow - Power object
+console.log(Math.pow(2, 3)); // 8
+//or
+console.log(2 ** 3); // 8
+
+// remember that and JS object is a place to store stuff
---
+++ 4-Semantics/32-WhatMattersThisSection.md
@@ -0,0 +1,16 @@
+# What Matters This Section
+
+## Crucial
+
+- Understanding What HTML5 Is
+- Block Vs. Inline Elements
+- <span> and <div> elements
+- Sementic Elements
+
+## Nice To Haves
+
+- <sub> element
+- <hr> element
+- <sup> element
+- <br> element
+- VSCode Tip: Emmet
