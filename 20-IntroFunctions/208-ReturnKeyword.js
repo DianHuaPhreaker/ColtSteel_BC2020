b2a7507004c8b54dd6e11baabfde5987ee95c520
dianhuaphreaker <ericqyhan@outlook.com>
2020-11-16 22:44:30 -0500

    coding exercises

--- 20-IntroFunctions/208-ReturnKeyword.js
+++ 20-IntroFunctions/208-ReturnKeyword.js
@@ -4,7 +4,18 @@ function add(x, y){
     let sum = x + y;
     return sum;
 }
-
+// remember that return keywowrds stops run of function
 console.log(add(9, 5)
 );
 
+
+function add(x, y){
+    if (typeof x!== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    let sum = x + y;
+    return sum;
+}
+
+
+console.log(add(add(1,5), 9));
---
+++ 20-IntroFunctions/codingExercise.js
@@ -0,0 +1,46 @@
+// Coding Exercise 38
+function multiply(x, y){
+    return x * y; 
+}
+
+console.log(multiply(2, 3));
+console.log(multiply(9, 9));
+console.log(multiply(5, 4));
+
+// Coding Exercise 39
+function isShortsWeather(temperature) {
+    if (temperature >= 75){
+        return true
+    } return false; 
+}
+
+console.log(isShortsWeather(75));
+console.log(isShortsWeather(76));
+console.log(isShortsWeather(74));
+
+// Coding Exercise 40
+function lastElement()
+
+
+// Coding Exercise 43
+function returnDay(day){
+    if (day === 1){
+        return "Monday";
+    } else if (day === 2){
+        return "Tuesday"
+    } else if (day === 3){
+        return "Wednesday"
+    } else if (day === 4){
+        return "Thursday"
+    } else if (day === 5){
+        return "Friday"
+    } else if (day === 6){
+        return "Saturday"
+    } else if (day === 7){
+        return "Sunday"
+    } else if (day > 7 || day < 0){
+        return "null"
+    }
+}
+
+console.log(returnDay(9));
