923eb4e319df239596db566828eca123db969ce9
dianhuaphreaker <ericqyhan@outlook.com>
2020-11-24 23:35:41 -0500

    minor update

---
+++ 21-LevelingUpFunctions/214-HigherOrderFunction.js
@@ -0,0 +1,21 @@
+// Higher Order Functions 
+function callTwice(func){
+    func();
+    func();
+}
+
+function callTenTimes(f){
+    for(let i = 0; i < 10; i++){
+        f()
+    }
+}
+
+
+function rollDie() {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    console.log(roll);
+}
+
+
+console.log(callTwice(rollDie));
+console.log(callTenTimes(rollDie));
